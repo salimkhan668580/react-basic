a38c4e3f1b8de957ee2cac93817cbb4d4a1d707f
salimkhan668580 <salimkhan668580s@gmail.com>
2023-11-28 21:32:44 +0530

    add functionality

--- CommentImpliment/src/Component/Comment.jsx
+++ CommentImpliment/src/Component/Comment.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Comment() {
+    let [formData,setFormData]=useState({
+      userName:"",
+      remark:"",
+      rating:5
+    });
+
+    let formHandler=(e)=>{
+        setFormData((currData)=>{
+      
+            return {...currData,[e.target.name]:e.target.value};
+         });
+    };
+
+    let formSubmit=(e)=>{
+        e.preventDefault();
+        console.log(formData);
+        setFormData({
+            userName:"",
+            remark:"",
+            rating:5
+        });
+    }
+
+
   return (
-    <div><h1>This is comment</h1></div>
+   <>
+   <h1>Add Some Comment</h1>
+
+   <form onSubmit={formSubmit}>
+    <input type="text" placeholder='UserName' name='userName' onChange={formHandler} value={formData.userName}/>
+    <br />
+    <input type="text" placeholder='Remark' name='remark' onChange={formHandler} value={formData.remark}/>
+    <br />
+    <input type="number"  min={1} max={5} onChange={formHandler} name='rating' value={formData.rating}/>
+    <br /><br />
+    <button>Submit</button>
+   </form>
+   
+   
+   </>
   )
 }
 
