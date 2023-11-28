378590ec5cfb565f5f54250919e02f75bee55d3f
salimkhan668580 <salimkhan668580s@gmail.com>
2023-11-28 21:10:40 +0530

    Comment add

---
+++ CommentImpliment/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Comment from './Component/Comment'
+
+function App() {
+
+
+  return (
+    <>
+   <Comment/>
+    </>
+  )
+}
+
+export default App
---
+++ CommentImpliment/src/Component/Comment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Comment() {
+  return (
+    <div><h1>This is comment</h1></div>
+  )
+}
+
+export default Comment
